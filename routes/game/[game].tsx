7ee0989bd1cfb0131cebb84412bcd007c7ac2095
M3nny <m3nny@protonmail.com>
2022-07-14 11:52:32 +0200

    Add video player

--- routes/game/[game].tsx
+++ routes/game/[game].tsx
@@ -15,8 +15,13 @@ export const handler: Handlers = {
         let game = await free_games.filter(function(item) {
             return item.title === ctx.params.game.replace(/%20/g, ' ');
         })
+        let yt_api_key = '';
+        let query: string = await fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + game[0].title + ' Official Trailer&type=video&key=' + yt_api_key);
+        let yt_json_api = await query.json();
+        console.log(yt_json_api.items[0].id.videoId);
+        game.push('https://www.youtube.com/embed/' + yt_json_api.items[0].id.videoId);
         return ctx.render(game);
-  }
+    }
 }
 
 export default function Game(props) {
@@ -24,8 +29,8 @@ export default function Game(props) {
         <body class={tw`bg-base font-sans`}>
             <title>Weeky Games - {props.params.game.replace(/%20/g, ' ')}</title>
             <Navbar/>
-
             
+            <iframe width="560" height="315" src={props.data[1]} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </body>
     )
 }
